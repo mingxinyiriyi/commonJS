546cb72526210828c0f3d75a9f9d70bdab6b26ec
ming <minghengccit@126.com>
2020-12-01 10:16:55 +0800

    xxx

---
+++ js/src/module1.js
@@ -0,0 +1,6 @@
+//暴露一个对象
+module.exports ={
+    foo(){
+        console.log('module1 foo()');
+    }
+}
---
+++ js/src/module3.js
@@ -0,0 +1,8 @@
+//分别暴露
+exports.foo = function(){
+    console.log('module3 foo()');
+}
+
+exports.bar = function(){
+    console.log('module3 bar()');
+}
